f2b781a6873ad208ff57c5cf31474bc11950871d
Gavin Gavington <GavinGavington@gavins-macbook-air.local>
2018-09-30 10:20:08 -0700

    Added program.

--- Monster/main.cpp
+++ Monster/main.cpp
@@ -6,9 +6,58 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+enum class Monsters
+{
+    OGRE,
+    DRAGON,
+    ORC,
+    GIANT_SPIDER,
+    SLIME
+};
+
+struct monster
+{
+    Monsters monsterType;
+    std::string name;
+    int health;
+};
+std::string printMonsterType(Monsters enumMonsterType)
+{
+    if (static_cast<int>(enumMonsterType) == 0)
+    {
+        return "Ogre";
+    }
+    if (static_cast<int>(enumMonsterType) == 1)
+    {
+        return "Dragon";
+    }
+    if (static_cast<int>(enumMonsterType) == 2)
+    {
+        return "Orc";
+    }
+    if (static_cast<int>(enumMonsterType) == 3)
+    {
+        return "Giant Spider";
+    }
+    if (static_cast<int>(enumMonsterType) == 4)
+    {
+        return "Slime";
+    }
+    return "error";
+}
+void printMonster(monster monsterX)
+{
+    
+    std::cout << "This " << printMonsterType(monsterX.monsterType) << " is named " << monsterX.name << " and has " << monsterX.health << " health." << std::endl;
+}
+int main()
+{
+    
+    monster monsterOne {Monsters::OGRE, "Torg", 145};
+    monster monsterTwo {Monsters::SLIME, "Blurp", 23};
+    printMonster(monsterOne);
+    printMonster(monsterTwo);
     return 0;
 }
